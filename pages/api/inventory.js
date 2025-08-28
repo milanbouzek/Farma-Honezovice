3753aa62672ec6afb9a9d3477e707eec0bcadc2b
milanbouzek <milan.bouzek@icloud.com>
2025-08-28 11:05:01 +0200

    Create inventory.js

---
+++ pages/api/inventory.js
@@ -0,0 +1,10 @@
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), "public", "inventory.json");
+  const jsonData = fs.readFileSync(filePath, "utf-8");
+  const data = JSON.parse(jsonData);
+
+  res.status(200).json(data);
+}
