99868c9260d057a78813f41790690a96fa85f7d4
milanbouzek <milan.bouzek@icloud.com>
2025-08-29 14:18:19 +0200

    Update test-smtp.js

--- pages/api/test-smtp.js
+++ pages/api/test-smtp.js
@@ -12,10 +12,10 @@ export default async function handler(req, res) {
     },
   });
 
-  // Debug – vypíše env proměnné
-  console.log("SMTP_HOST:", process.env.SMTP_HOST);
-  console.log("SMTP_USER:", process.env.SMTP_USER);
-  console.log("SMTP_PASS:", process.env.SMTP_PASS ? "********" : undefined);
+ console.log("SMTP_HOST:", process.env.SMTP_HOST);
+console.log("SMTP_USER:", process.env.SMTP_USER);
+console.log("SMTP_PASS:", process.env.SMTP_PASS ? "********" : undefined);
+
 
   try {
     // Test připojení k SMTP
