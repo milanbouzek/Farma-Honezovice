324b59624c86441ad116ecd3271376d741267ca4
milanbouzek <milan.bouzek@icloud.com>
2025-08-29 10:51:20 +0200

    Update order.js

--- pages/api/order.js
+++ pages/api/order.js
@@ -1,6 +1,7 @@
 // pages/api/order.js
 import { supabase } from '../../lib/supabaseClient';
 
+
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { name, email, quantity } = req.body;
