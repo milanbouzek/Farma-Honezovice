bb239ad7def7eb8e4d1e0a48a51088abb1ed91c5
milanbouzek <milan.bouzek@icloud.com>
2025-09-02 16:26:44 +0200

    Update objednavka.js

--- pages/objednavka.js
+++ pages/objednavka.js
@@ -1,25 +1,16 @@
 import Layout from "../components/Layout";
-import { useState } from "react";
 import OrderForm from "../components/OrderForm";
 
 export default function Objednavka() {
-  const [showForm, setShowForm] = useState(false);
-
   return (
     <Layout>
       <h1 className="text-3xl font-bold text-green-700 mb-4">Objednávka vajec</h1>
       <p className="text-gray-700 mb-6">
-        Klikněte na tlačítko níže pro zadání objednávky přímo na našem webu.
+        Vyplňte níže formulář a objednejte si čerstvá vejce přímo z naší farmy.
       </p>
-      {!showForm && (
-        <button
-          onClick={() => setShowForm(true)}
-          className="bg-yellow-400 text-gray-900 font-semibold px-6 py-3 rounded-xl shadow-md hover:bg-yellow-500 hover:scale-105 transform transition duration-300"
-        >
-          Otevřít objednávkový formulář
-        </button>
-      )}
-      {showForm && <OrderForm />}
+
+      {/* Formulář je nyní zobrazený přímo */}
+      <OrderForm />
     </Layout>
   );
 }
