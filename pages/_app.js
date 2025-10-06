5a10a9a4e3f3176616b3ac066f05fbe200ea141c
milanbouzek <milan.bouzek@icloud.com>
2025-10-06 12:43:21 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+// pages/_app.js
 import "../styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import { AdminAuthProvider } from "../components/AdminAuthContext";
@@ -6,27 +7,8 @@ export default function App({ Component, pageProps }) {
   return (
     <AdminAuthProvider>
       <Component {...pageProps} />
-      <Toaster
-        position="top-center"
-        toastOptions={{
-          style: {
-            borderRadius: "16px",
-            background: "#fff8dc",
-            color: "#333",
-            fontSize: "18px",
-            padding: "24px",
-            minWidth: "300px",
-            maxWidth: "500px",
-            boxShadow: "0 6px 20px rgba(0,0,0,0.2)",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-          },
-          success: { duration: 5000 },
-          error: { duration: 6000 },
-        }}
-      />
+      {/* Toaster (můžeš upravit styl jak chceš) */}
+      <Toaster position="top-center" />
     </AdminAuthProvider>
   );
 }
