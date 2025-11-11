13a3be381efbca2eb262faa9368055423df46faf
milanbouzek <milan.bouzek@icloud.com>
2025-11-11 07:42:46 +0100

    Update confirm.js

--- pages/api/preorders/confirm.js
+++ pages/api/preorders/confirm.js
@@ -1,5 +1,5 @@
 // pages/api/preorders/confirm.js
-import { supabase } from "../../../utils/supabaseClient";
+import supabase from "../../../lib/supabaseClient";
 
 export default async function handler(req, res) {
   if (req.method !== "PATCH") {
