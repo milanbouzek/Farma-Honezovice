d6f43d951729fd4c61bcb2001d2164de21694085
milanbouzek <milan.bouzek@icloud.com>
2025-11-12 21:02:50 +0100

    Update create.js

--- pages/api/preorders/create.js
+++ pages/api/preorders/create.js
@@ -1,3 +1,6 @@
+console.log("Received body:", req.body);
+console.log("Parsed quantities:", Number(req.body.standardQty), Number(req.body.lowcholQty));
+
 import { supabase } from "@/lib/supabaseClient";
 
 export default async function handler(req, res) {
